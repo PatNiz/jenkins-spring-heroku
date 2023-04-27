60514cab29b218f7f182144154994534f0cb3156
Patryk Niziołek <patryk.niziolek@ttpsc.pl>
2023-04-27 23:08:34 +0200

    Initial commit

---
+++ src/main/java/pl/niziolekp/jenkinsspringheroku/JenkinsSpringHerokuApplication.java
@@ -0,0 +1,19 @@
+package pl.niziolekp.jenkinsspringheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsSpringHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringHerokuApplication.class, args);
+	}
+	@GetMapping("/")
+	public String get(){
+		return "Hello(simple project Docker+Jenkins+Junit)";
+	}
+}
